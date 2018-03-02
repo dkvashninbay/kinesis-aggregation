3182127099024d33c1cf99f8cf5464758da0708a
Dmytro Kvashnin <dkvashnin@spscommerce.com>
2018-03-02 15:39:40 +0200

    Name change

--- python/setup.py
+++ python/setup.py
@@ -24,8 +24,8 @@ def read_file(filename):
 
 
 setup(
-  name='aws_kinesis_agg',
-  packages=['aws_kinesis_agg'],
+  ame='aws_kinesis_agg_sps',
+  packages=['aws_kinesis_agg_sps'],
   version='1.0.1',
   description='Python module to assist in taking advantage of the Kinesis message aggregation '
               'format for both aggregation and deaggregation.',
